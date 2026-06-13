69fba9416966d31c7d6cc87286122bb5293d5c34
Bip901 <74911767+Bip901@users.noreply.github.com>
2026-06-13 10:56:09 +0300

    Refactor EncryptionKeysCrypto

--- CsCryptFs/EncryptionKeysCrypto.cs
+++ CsCryptFs/EncryptionKeysCrypto.cs
@@ -13,48 +13,42 @@ internal static class EncryptionKeysCrypto
     private const string HKDFInfoGCMContent = "AES-GCM file content encryption";
     private const int IV_LENGTH = 16;
     private const int AD_LENGTH = 8;
+    private const int MAC_SIZE_BITS = 16 * 8; // 128 bits
 
     public static byte[] EncryptKey(byte[] kek, byte[] masterKey)
     {
-        byte[] aeadKey = GetFileContentEncryptionKey(kek);
-        try
-        {
-            GcmBlockCipher cipher = new(new AesEngine());
-            byte[] nonce = RandomNumberGenerator.GetBytes(IV_LENGTH);
-            AeadParameters parameters = new(new KeyParameter(aeadKey), 128, nonce, associatedText: new byte[AD_LENGTH]);
-            cipher.Init(true, parameters);
-
-            byte[] result = new byte[nonce.Length + cipher.GetOutputSize(masterKey.Length)];
-            Array.Copy(nonce, result, nonce.Length);
-            int len = cipher.ProcessBytes(masterKey, result.AsSpan(nonce.Length..));
-            cipher.DoFinal(result.AsSpan((nonce.Length + len)..));
-            return result;
-        }
-        finally
-        {
-            Array.Clear(aeadKey, 0, aeadKey.Length);
-        }
+        byte[] nonce = RandomNumberGenerator.GetBytes(IV_LENGTH);
+        GcmBlockCipher cipher = GetGcmBlockCipher(kek, nonce, forEncryption: true);
+        byte[] result = new byte[nonce.Length + cipher.GetOutputSize(masterKey.Length)];
+        nonce.CopyTo(result);
+        int len = cipher.ProcessBytes(masterKey, result.AsSpan(nonce.Length..));
+        cipher.DoFinal(result.AsSpan((nonce.Length + len)..));
+        return result;
     }
 
     public static byte[] DecryptKey(byte[] kek, byte[] encryptedMasterKey)
     {
-        byte[] aeadKey = GetFileContentEncryptionKey(kek);
-        try
-        {
-            GcmBlockCipher cipher = new(new AesEngine());
-            byte[] nonce = encryptedMasterKey[..IV_LENGTH];
-            AeadParameters parameters = new(new KeyParameter(aeadKey), 128, nonce, associatedText: new byte[AD_LENGTH]);
-            cipher.Init(false, parameters);
+        byte[] nonce = encryptedMasterKey[..IV_LENGTH];
+        GcmBlockCipher cipher = GetGcmBlockCipher(kek, nonce, forEncryption: false);
+        byte[] result = new byte[cipher.GetOutputSize(encryptedMasterKey.Length - nonce.Length)];
+        int len = cipher.ProcessBytes(encryptedMasterKey.AsSpan(nonce.Length), result);
+        cipher.DoFinal(result.AsSpan(len));
+        return result;
+    }
 
-            byte[] result = new byte[cipher.GetOutputSize(encryptedMasterKey.Length - nonce.Length)];
-            int len = cipher.ProcessBytes(encryptedMasterKey.AsSpan(nonce.Length), result);
-            cipher.DoFinal(result.AsSpan(len));
-            return result;
-        }
-        finally
-        {
-            Array.Clear(aeadKey, 0, aeadKey.Length);
-        }
+    private static GcmBlockCipher GetGcmBlockCipher(byte[] kek, byte[] nonce, bool forEncryption)
+    {
+        byte[] aeadKey = GetFileContentEncryptionKey(kek);
+        AeadParameters parameters = new(
+            new KeyParameter(aeadKey),
+            MAC_SIZE_BITS,
+            nonce,
+            associatedText: new byte[AD_LENGTH]
+        );
+
+        GcmBlockCipher cipher = new(new AesEngine());
+        cipher.Init(forEncryption, parameters);
+        return cipher;
     }
 
     public static byte[] GetFilenameEncryptionKey(byte[] masterKey)
@@ -62,18 +56,17 @@ internal static class EncryptionKeysCrypto
         return HkdfDerive(masterKey, HKDFInfoEMENames);
     }
 
-    public static byte[] GetFileContentEncryptionKey(byte[] masterKey)
+    public static byte[] GetFileContentEncryptionKey(byte[] key)
     {
-        return HkdfDerive(masterKey, HKDFInfoGCMContent);
+        return HkdfDerive(key, HKDFInfoGCMContent);
     }
 
     /// <summary>
-    /// Derives "outLen" bytes from "masterKey" and "info" using HKDF-SHA256.
+    /// Derives <paramref name="outLen"/> bytes from <paramref name="key"/> and <paramref name="info"/> using HKDF-SHA256.
     /// </summary>
-    /// <exception cref="CryptographicException"/>
-    private static byte[] HkdfDerive(byte[] masterKey, string info, int outLen = 32)
+    private static byte[] HkdfDerive(byte[] key, string info, int outLen = 32)
     {
         byte[] infoBytes = Encoding.UTF8.GetBytes(info);
-        return HKDF.DeriveKey(HashAlgorithmName.SHA256, masterKey, outLen, salt: null, info: infoBytes);
+        return HKDF.DeriveKey(HashAlgorithmName.SHA256, key, outLen, salt: null, info: infoBytes);
     }
 }
